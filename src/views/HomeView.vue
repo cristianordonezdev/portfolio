b0daac3089eadf05e99c12c00dc676a1ce6c9ab6
Cristian Hernandez <chinorubikguitar@hotmail.com>
2024-07-08 19:28:06 -0600

    test quit style from home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <main class="text-white main-container-home bg-danger">
+  <main class="text-white main-container-home">
     <div class="main-title">Cristian Ordoñez</div>
     <p class="main-dedicate">Full-Stack Developer</p>
   </main>
